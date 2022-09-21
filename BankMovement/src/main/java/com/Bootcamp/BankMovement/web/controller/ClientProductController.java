65c86d98fc76fcc08b5124b915e55dd932432f1a
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2022-09-21 12:25:10 -0500

    Pasar a reactivo el ms Movement

--- BankMovement/src/main/java/com/Bootcamp/BankMovement/web/controller/ClientProductController.java
+++ BankMovement/src/main/java/com/Bootcamp/BankMovement/web/controller/ClientProductController.java
@@ -1,10 +1,11 @@
 package com.Bootcamp.BankMovement.web.controller;
 
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Bootcamp.BankMovement.domain.ClientProduct;
 import com.Bootcamp.BankMovement.service.IClientProductService;
-import com.Bootcamp.BankMovement.web.model.ClientProductModel;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -27,88 +27,45 @@ import com.Bootcamp.BankMovement.web.model.ClientProductModel;
 @Slf4j
 public class ClientProductController {
 
-    @Autowired
-    private final IClientProductService clientProductService;
+	@Autowired
+	private final IClientProductService clientProductService;
 
+	@GetMapping()
+	@Operation(summary = "Get list of clientProducts")
+	public Flux<ClientProduct> getAll() throws Exception {
+		log.info("getAll" + "OK");
+		log.debug(HttpStatus.OK.toString());
+		return clientProductService.findAll();
+	}
 
-    /**
-     * Get list of clientProducts
-     * @return
-     * @throws Exception
-     */
-    @GetMapping()
-    @Operation(summary = "Get list of clientProducts")
-    public ResponseEntity<Object> getAll() throws Exception {
-        List<ClientProductModel> response =  clientProductService.findAll();
-        log.info("getAll" + "OK");
-        log.debug(response.toString());
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+	@GetMapping(path = { "{id}" }, produces = { "application/json" })
+	public ResponseEntity<Mono<ClientProduct>> getById(@PathVariable("id") String id) throws Exception {
+		Mono<ClientProduct> response = clientProductService.findById(id);
+		log.info("getById" + "OK");
+		log.debug(id.toString());
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
 
-    /**
-     * Get clientProduct by id
-     * @param id
-     * @return
-     * @throws Exception
-     */
-    @GetMapping(path = { "{id}" }, produces = { "application/json" })
-    public ResponseEntity<ClientProductModel> getById(@PathVariable("id") String id) throws Exception{
-        ClientProductModel response = clientProductService.findById(id);
-        log.info("getById" + "OK");
-        log.debug(id.toString());
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+	@PostMapping(path = "/create")
+	public Mono<ClientProduct> create(@RequestBody ClientProduct clientProductModel) throws Exception {
+			log.info("create" + "OK");
+			log.debug(clientProductModel.toString());
+			return clientProductService.create(clientProductModel);
+	}
 
-    /**
-     * Create clientProduct
-     * @param clientProductModel
-     * @return
-     * @throws Exception
-     */
-    @PostMapping(path = "/create")
-    public ResponseEntity<Object> create(@RequestBody ClientProductModel clientProductModel) throws Exception {
+	@PutMapping(path = { "{id}" }, produces = { "application/json" })
+	public Mono<ClientProduct> update(@PathVariable("id") String id, @RequestBody ClientProduct clientProductModel)
+			throws Exception {
+		log.info("update" + "OK");
+		log.debug(id.toString() + "/" + clientProductModel.toString());
+		return clientProductService.update(id, clientProductModel);
+	}
 
-        try{
-            ClientProductModel response = clientProductService.create(clientProductModel);
-
-
-            log.info("create" + "OK");
-            log.debug(clientProductModel.toString());
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-        catch (Exception e){
-
-            return new ResponseEntity<>("Error:"+e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-
-    /**
-     * Update clientProduct by id
-     * @param id
-     * @param clientProductModel
-     * @throws Exception
-     */
-    @PutMapping(path = { "{id}" }, produces = { "application/json" })
-    public void update(
-            @PathVariable("id") String id,
-            @RequestBody ClientProductModel clientProductModel) throws Exception {
-        clientProductService.update(id, clientProductModel);
-        log.info("update" + "OK");
-        log.debug(id.toString()+ "/" + clientProductModel.toString());
-    }
-
-    /**
-     * Delete clientProduct by id
-     * @param id
-     * @throws Exception
-     * @author aangulom
-     */
-    @DeleteMapping({ "{id}" })
-    public void deleteById(@PathVariable("id") String id) throws Exception {
-        clientProductService.deleteById(id);
-        log.info("deleteById" + "OK");
-        log.debug(id.toString());
-    }
+	@DeleteMapping({ "{id}" })
+	public void deleteById(@PathVariable("id") String id) throws Exception {
+		clientProductService.deleteById(id);
+		log.info("deleteById" + "OK");
+		log.debug(id.toString());
+	}
 
 }

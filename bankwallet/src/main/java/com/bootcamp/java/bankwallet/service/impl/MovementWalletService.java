c23b404e4e9dbaf052e1af153c9614fe026de02d
sjhilmar <sjalvarezt9627@gmail.com>
2022-09-22 23:43:38 -0500

    Update MovementWalletService.java

--- bankwallet/src/main/java/com/bootcamp/java/bankwallet/service/impl/MovementWalletService.java
+++ bankwallet/src/main/java/com/bootcamp/java/bankwallet/service/impl/MovementWalletService.java
@@ -8,10 +8,12 @@ import com.bootcamp.java.bankwallet.domain.MovementWallet;
 import com.bootcamp.java.bankwallet.repository.MovementWalletReposiroty;
 import com.bootcamp.java.bankwallet.repository.WalletClientReposiroty;
 import com.bootcamp.java.bankwallet.service.IMovementWalletService;
+import com.bootcamp.java.bankwallet.util.Constants;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 @Service
 @RequiredArgsConstructor
 public class MovementWalletService implements IMovementWalletService {
@@ -19,7 +21,7 @@ public class MovementWalletService implements IMovementWalletService {
 	private MovementWalletReposiroty repository;
 	@Autowired
 	private WalletClientReposiroty clientRepository;
- 	
+
 	@Override
 	public Flux<MovementWallet> findAll() throws Exception {
 		return repository.findAll();
@@ -30,31 +32,45 @@ public class MovementWalletService implements IMovementWalletService {
 
 		return repository.findById(id).switchIfEmpty(Mono.error(new Exception("Movement didn´t find")));
 	}
-
+/*
+ * Finding all clients and filter by phone number
+ * Adding data to movment
+ * Testing if a deposit or withdrawal
+ * saving data*/
 	@Override
-	public Mono<MovementWallet> create(String phoneNumber,MovementWallet movement) throws Exception {
-	return clientRepository.findAll()
-			.filter(t ->t.getPhoneNumber().equalsIgnoreCase(phoneNumber))
-			.switchIfEmpty(Mono.error(new Exception("Number Phone doesn't exists")))
-			.collectList()
-			.flatMap(t -> {
-				if (!t.isEmpty()) {
-					movement.setWalletClient(t.get(0));
-					t.get(0).setBalance(movement.getAmount());
-					return repository.save(movement);
-				}else {
-					return Mono.error(new Exception( HttpStatus.BAD_REQUEST.toString()));
-				}
-			});
-		
+	public Mono<MovementWallet> create(String phoneNumber, MovementWallet movement) throws Exception {
+		return clientRepository.findAll().filter(t -> t.getPhoneNumber().equalsIgnoreCase(phoneNumber))
+				.switchIfEmpty(Mono.error(new Exception("Number Phone doesn't exists")))
+				.collectList()
+				.flatMap(t -> {
+					if (!t.isEmpty()) {
+						movement.setWalletClient(t.get(0));
+						//t.get(0).setBalance(movement.getAmount());
+						return Mono.just(t.get(0));
+					} else {
+						return Mono.error(new Exception(HttpStatus.BAD_REQUEST.toString()));
+					}
+				}).flatMap(t ->{
+					if (movement.getType().equalsIgnoreCase(Constants.MOVEMENT_TYPE_DEPOSIT)) {
+						return repository.save(movement);
+					}else if (movement.getType().equalsIgnoreCase(Constants.MOVEMENT_TYPE_WITHDRAWAL)) {
+						if (movement.getAmount()<= t.getBalance()) {
+							return repository.save(movement);
+						}else {
+							return Mono.error(new Exception("Amount is higher than banlance"));
+						}
+					}else{
+						return Mono.error(new Exception(HttpStatus.BAD_REQUEST.toString()));
+					}
+					
+				});
 	}
 
 	@Override
 	public Mono<MovementWallet> update(String id, MovementWallet movement) throws Exception {
 
-		return repository.findById(id)
-				.switchIfEmpty(Mono.error(new Exception("Movement didn´t exists")))
-				.flatMap(t -> repository.save(movement) );
+		return repository.findById(id).switchIfEmpty(Mono.error(new Exception("Movement didn´t exists")))
+				.flatMap(t -> repository.save(movement));
 	}
 
 	@Override
